2eb3419c51d6b33d862349c96d11f3fa38fec271
Temmycoder <oladutemutemiloluwa@icloud.com>
2025-07-22 22:04:12 +0100

    backend integrated for contact form

---
+++ contact.php
@@ -0,0 +1,29 @@
+<?php 
+
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'temmycoder';
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+if(isset($_POST['submit'])){
+  $fname = $_POST['fname'];
+  $lname = $_POST['lname'];
+  $email = $_POST['email'];
+  $phone = $_POST['phone'];
+  $msg = $_POST['msg'];
+
+  $query = mysqli_query($conn, "INSERT INTO clients (first_name, last_name, email, phone, message) 
+  values ('$fname', '$lname', '$phone', '$email', '$msg')");
+
+  if($query){
+    header("location: index.html");
+  }else{
+    echo "failed to insert ";
+  }
+}
